83f0b4c939fb62bf3319dcb8346d45fc1b7cb107
thanhlong201 <ngay9thang12nam2001@gmail.com>
2021-05-26 20:02:59 +0700

    Upload Project first time

---
+++ DoAn/DoAn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DoAn.Startup))]
+namespace DoAn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
