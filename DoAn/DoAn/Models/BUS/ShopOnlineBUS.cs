86cab696d91fcc712bcef83a8427f39b445485cf
thanhlong201 <ngay9thang12nam2001@gmail.com>
2021-05-28 15:31:57 +0700

    Product _Details_PhanTrang

---
+++ DoAn/DoAn/Models/BUS/ShopOnlineBUS.cs
@@ -0,0 +1,23 @@
+﻿using ShopOnLineConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoAn.Models.BUS
+{
+    public class ShopOnlineBUS
+    {
+        public static IEnumerable<SanPham> DanhSach()
+        {
+            var db = new ShopOnLineConnectionDB();
+            return db.Query<SanPham>("select * from  SanPham where TinhTrang = 0");
+        }
+        public static SanPham ChiTiet(String a)
+        {
+            var db = new ShopOnLineConnectionDB();
+            return db.SingleOrDefault<SanPham>("select * from  SanPham where MaSanPham = @0",a);
+        }
+
+    }
+}
